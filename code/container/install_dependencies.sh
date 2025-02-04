388a1a833734568c91aa799a445ac4547d1e9259
Marvin Buss <marvin.buss@gmail.com>
2025-02-04 01:34:27 +0100

    Test without sudo

--- code/container/install_dependencies.sh
+++ code/container/install_dependencies.sh
@@ -3,20 +3,20 @@ AZURE_CLI_VERSION=$1
 PWSH_VERSION=$2
 
 # Install Azure CLI
-sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg \
-    && sudo mkdir -p /etc/apt/keyrings \
-    && curl -sLS https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null \
-    && sudo chmod go+r /etc/apt/keyrings/microsoft.gpg \
+apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg \
+    && mkdir -p /etc/apt/keyrings \
+    && curl -sLS https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/keyrings/microsoft.gpg > /dev/null \
+    && chmod go+r /etc/apt/keyrings/microsoft.gpg \
     && AZ_DIST=$(lsb_release -cs) \
-    && echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_DIST main" | sudo tee /etc/apt/sources.list.d/azure-cli.list \
-    && sudo apt-get update \
+    && echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_DIST main" | tee /etc/apt/sources.list.d/azure-cli.list \
+    && apt-get update \
     && AZ_DIST=$(lsb_release -cs) \
-    && sudo apt-get install -y azure-cli=$AZURE_CLI_VERSION-1~$AZ_DIST
+    && apt-get install -y azure-cli=$AZURE_CLI_VERSION-1~$AZ_DIST
 
 # Install Powershell
-sudo apt-get install -y wget \
+apt-get install -y wget \
     && wget https://github.com/PowerShell/PowerShell/releases/download/v$PWSH_VERSION/powershell_$PWSH_VERSION-1.deb_amd64.deb \
-    && sudo dpkg -i powershell_$PWSH_VERSION-1.deb_amd64.deb \
-    && sudo apt-get install -fy \
+    && dpkg -i powershell_$PWSH_VERSION-1.deb_amd64.deb \
+    && apt-get install -fy \
     && rm powershell_$PWSH_VERSION-1.deb_amd64.deb \
     && pwsh -Command "Install-Module -Name Az -Repository PSGallery -Force"
