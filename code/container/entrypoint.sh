8789fe9631d881756a4f173ef1b609f40c7e2ba1
Marvin Buss <marvin.buss@gmail.com>
2025-02-10 16:58:55 +0100

    Update image

--- code/container/entrypoint.sh
+++ code/container/entrypoint.sh
@@ -3,10 +3,12 @@
 
 # Configure SSL certificates
 # export GITHUB_ACTIONS_RUNNER_TLS_NO_VERIFY=1
-openssl s_client -showcerts -connect github.com:443 </dev/null 2>/dev/null|openssl x509 -outform PEM > /mycert.pem
-openssl x509 -outform der -in mycert.pem -out /usr/local/share/ca-certificates/mycert.crt
-# sudo security add-trusted-cert -d -r trustAsRoot -k /mycert.crt
+openssl s_client -showcerts -connect github.com:443 </dev/null 2>/dev/null | sed -e '/-----BEGIN/,/-----END/!d' | tee "/usr/local/share/ca-certificates/ca.crt" >/dev/null
 sudo update-ca-certificates
+# openssl s_client -showcerts -connect github.com:443 </dev/null 2>/dev/null| openssl x509 -outform PEM > /mycert.pem
+# openssl x509 -outform der -in mycert.pem -out /usr/local/share/ca-certificates/mycert.crt
+# sudo security add-trusted-cert -d -r trustAsRoot -k /mycert.crt
+# sudo update-ca-certificates
 
 export RUNNER_ALLOW_RUNASROOT=1
 export PATH=${PATH}:/actions-runner
